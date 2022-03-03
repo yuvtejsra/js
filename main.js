f5a88b02e5ef2aa4716e598cabc42fe9c2fcb045
yuvtejsra <72407821+yuvtejsra@users.noreply.github.com>
2022-03-02 17:01:06 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var naruto="sasuke";
+console.log(naruto);
+var goku="over 90000000";
+console.log(goku);
+var Eren_Jaeger=naruto.concat(goku);
+console.log(Eren_Jaeger);
+var length_Eren=Eren_Jaeger.length;
+console.log(length_Eren);
+var kakashi_sensei="son of the white fang";
+console.log(kakashi_sensei);
+var replacesensei=kakashi_sensei.replace("son","sun");
+console.log(replacesensei);
+
+var number_1=7;
+console.log(number_1);
+var number_2=43;
+console.log(number_2);
+var addition=number_1+number_2;
+console.log(addition);
+var multiply=number_1*number_2;
+console.log(multiply);
+
+
+
+
+
+
+
+
+
+
+
